ce41741e850d98e27175e2efc8b3f8009443037d
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-04-05 13:34:28 +0200

    Add and a draw loop

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const GRID_WIDTH = 60;
 const GRID_HEIGHT = 40;
+const layers = [];
+let canvas;
 
 const main = () => {
   console.log("Welcome to pixelator!");
 
   canvas = document.getElementById("main-canvas");
-  ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext("2d");
+  const initLayer = new PixelLayer(GRID_WIDTH, GRID_HEIGHT);
+  initLayer.active = true;
+  layers.push(initLayer);
 
   canvas.addEventListener("mousedown", canvasClick);
+  // draw once a second
+  const loop = setInterval(() => drawLoop(ctx), 1000);
 }
 
 const canvasClick = (event) => {
@@ -18,17 +25,59 @@ const canvasClick = (event) => {
   let gridX = getGridX(x);
   let gridY = getGridY(y);
 
-  ctx.fillRect(gridX, gridY, 10, 10);
+  layers[0].fillCell(gridX, gridY);
 }
 
 const getGridX = (x) => {
   const cellWidth = canvas.clientWidth / GRID_WIDTH;
-  return Math.floor(x / cellWidth) * cellWidth;
+  return Math.floor(x / cellWidth);
 }
 
 const getGridY = (y) => {
   const cellHeight = canvas.clientHeight / GRID_HEIGHT;
-  return Math.floor(y / cellHeight) * cellHeight;
+  return Math.floor(y / cellHeight);
+}
+
+const drawLoop = (ctx) => {
+  const cellWidth = canvas.clientWidth / GRID_WIDTH;
+  const cellHeight = canvas.clientHeight / GRID_HEIGHT;
+
+  layers.forEach(layer => {
+    if (!layer.active) {
+      return;
+    }
+    for (var i = 0; i < layer.width; i++) {
+      for (var j = 0; j < layer.height; j++) {
+        if (layer.cells[j * layer.width + i] === 1) {
+          ctx.fillRect(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
+        }
+      }
+    }
+  });
+}
+
+class PixelLayer {
+  width;
+  height;
+  cells = [];
+  active;
+
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    for (var i = 0; i < this.height * this.width; i++) {
+      this.cells[i] = 0;
+    }
+    this.active = false;
+  }
+
+  fillCell(x, y) {
+    if (x < 0 || y < 0 || x > this.width || y > this.height) {
+      return;
+    }
+    // TODO: Add color info eventualy
+    this.cells[x + y * this.width] = 1;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", main)
